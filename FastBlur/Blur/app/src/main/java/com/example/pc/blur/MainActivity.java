bc1d4793eaf8229a075722298ab22792abbf5065
hrshsa <hrs123hrs@qq.com>
2016-10-24 11:39:22 +0800

    11111

---
+++ FastBlur/Blur/app/src/main/java/com/example/pc/blur/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.pc.blur;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    private ImageView imageView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.image);
+        button = (Button) findViewById(R.id.button);
+        imageView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                imageView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+
+            }
+
+
+        });
+    }
+
+    public void click(View view ) {
+        FastBlur.applyBlur(imageView);
+
+    }
+}
